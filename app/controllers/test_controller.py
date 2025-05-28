3891ddca359b4487b99406a42fa260a682876f2b
jfavila09982 <jfavila09982@gmail.com>
2025-05-29 00:19:53 +0800

    feat: add test controller and routes , test upload file endpoint

--- app/controllers/test_controller.py
+++ app/controllers/test_controller.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+import shutil
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from app.utils.response_helper import reusable_response
+
+from pathlib import Path
+
 
 router = APIRouter()
 
@@ -19,9 +23,20 @@ def get_users():
     return reusable_response(users, message="This is test response")
 
 
-
-    
-    
-
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 
+async def handle_upload(file: UploadFile):
+    try:
+        file_location = UPLOAD_DIR / file.filename
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        return {"filename": file.filename, "message": "Upload successful"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Route handler
+@router.post("/test/upload")
+async def upload_file(file: UploadFile = File(...)):
+    return await handle_upload(file)
